37eed948f6b7bd50b475c0ea80536a6e42594c18
JSH1443 <inv1443@gmail.com>
2022-06-16 01:45:10 -0700

    feat : 변수 수정 및 코드 구조 수정작업 완료[JSH-SWITCH]

--- 3th/SHJ/Fan_Project/switch.c
+++ 3th/SHJ/Fan_Project/switch.c
@@ -3,63 +3,89 @@
 #include <avr/interrupt.h>
 #include <stdbool.h>
 
+#include "uart.h"
 
-unsigned int manual_flag1=0;
-unsigned int auto_flag1=0;
-unsigned int manual_flag2=0;
-unsigned int auto_flag2=0;
-unsigned int reset_timer =0;
 
-int switch_mode (int sw1, int sw2)
+unsigned int manual_mode_select=0;
+unsigned int auto_mode_select=0;
+unsigned int manual_mode_run=0;
+unsigned int auto_mode_run=0;
+
+
+void switch_mode (int intensity_sw, int action_sw)
 {
-   if(sw1%2==1 && manual_flag2==0 && auto_flag2==0)
+   if(intensity_sw%2==1 && manual_mode_run==0 && auto_mode_run==0)
     {
-     manual_flag1 =1;
-     auto_flag1 =0;
-
+     manual_mode_select =1;
+     auto_mode_select =0;
+     uart_string_trans("Manual Mode\n");
+     lcd_write_string("Manual Mode\n");
     }
 
-    else if (sw1%2==0 && manual_flag2==0 && auto_flag2==0)
+    else if (intensity_sw%2==0 && manual_mode_run==0 && auto_mode_run==0)
     {
-        auto_flag1 =1;
-        manual_flag1 =0;
-
+        auto_mode_select =1;
+        manual_mode_select =0;
+        uart_string_trans("Auto Mode\n");
+        lcd_write_string("Auto Mode\n");
     }
 
-   if (sw2==true && manual_flag1 ==1)
-    {
-        manual_flag2 =1;
-    }
-    else if(sw2==true && auto_flag1 ==1)
-    {
-        auto_flag2 =1;
-    }
-
-
-
-      if(sw2==3 && manual_flag2==1)
+      if(action_sw==2)
         {
-         manual_flag2=0;
-         manual_flag1=0;
-         sw1=0;
-         sw2=0;
-         uart_string_trans("Select Mode\n");
-        }
-       if(sw2==3 && auto_flag2==1 )
-        {
-         auto_flag2=0;
-         auto_flag1=0;
-         sw1=0;
-         sw2=0;
-         uart_string_trans("Select Mode\n");
+           servo_start();
         }
 
-    lcd_mode1(manual_flag1, auto_flag1);
-    lcd_mode2(manual_flag2, auto_flag2);
-    fan(sw1, sw2, manual_flag2);
-    manual_BLDC(sw1, manual_flag2, auto_flag2);
-    manual_servo(sw2, manual_flag2);
-    //temp_mode(auto_flag2);
-    //photocell_mode(auto_flag2);
+        if(action_sw==3)
+        {
+           servo_stop();
+        }
+
+   if (action_sw==1 && manual_mode_select ==1)
+    {
+        manual_mode_run =1;
+        uart_string_trans("Manual Mode run\n");
+        lcd_write_string("Manual Mode run\n");
+
+       if(intensity_sw==1)
+        {
+         bldc_low();
+        }
+
+       if(intensity_sw==2)
+        {
+         bldc_middle();
+        }
+
+        if(intensity_sw==3)
+        {
+         bldc_high();
+        }
+
+        if(intensity_sw==4)
+        {
+         bldc_stop();
+        }
+
+    }
+
+    else if(action_sw==1 && auto_mode_select ==1)
+    {
+        auto_mode_run =1;
+        uart_string_trans("Auto Mode run\n");
+        lcd_write_string("Auto Mode run\n");
+    }
+
+
+      if(action_sw==4 && (manual_mode_run==1 || auto_mode_run==1))
+        {
+            auto_mode_run=0;
+            auto_mode_select=0;
+            manual_mode_run=0;
+            manual_mode_select=0;
+            intensity_sw=0;
+            action_sw=0;
+            uart_string_trans("Select Mode\n");
+            lcd_write_string("Select Mode\n");
+        }
 }
 
