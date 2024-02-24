d1d1b887d47b04b87044d3ceb396c54ac7ab8259
oqazett <oqazett@gmail.com>
2024-02-24 11:11:59 +0900

    feat:[GwangSeon Eo] USART0 초기화 함수 추가 [DDD-PWM-6]

--- 5th/gseo/ddd_pwm/main.c
+++ 5th/gseo/ddd_pwm/main.c
@@ -2,10 +2,22 @@
  */
 
 #include <avr/io.h>
+#include <avr/sfr_defs.h>
+
+#include <util/delay.h>
+#include <stdio.h>
+
+#include "usart_printf.h"
 
 #define F_CPU       16000000L
-#include <util/delay.h>
 
+void LED_Toggle(void)
+{
+    PORTB = 0x00;
+    _delay_ms(500);
+    PORTB = 0x20;
+    _delay_ms(500);
+}
 
 int main(void)
 {
@@ -13,13 +25,16 @@ int main(void)
     // Insert code
     DDRB = 0x20;
 
-    while(1)
+    // TODO : USART0 Initialization for Putty Debug Message [DDD-PWM-6]
+    usart_initialize();
+    stdout = &uart_output;
+	_delay_ms(10);
+
+    for(;;)
     {
-        PORTB = 0x00;
-        _delay_ms(500);
-        PORTB = 0x20;
-        _delay_ms(500);
-    }
+		printf("Hello AVR printf\n");
+		_delay_ms(1000);
+	}
 
     return 0;
 }
---
+++ 5th/gseo/ddd_pwm/usart_printf.h
@@ -0,0 +1,12 @@
+#ifndef __USART_PRINTF_H__
+#define __USART_PRINTF_H__
+#include <stdio.h>
+
+void usart_send_byte(char byte, FILE *stream);
+char usart_get_byte(FILE *stream);
+void usart_initialize(void);
+
+FILE uart_output = FDEV_SETUP_STREAM(usart_send_byte, NULL, _FDEV_SETUP_WRITE);
+FILE uart_input = FDEV_SETUP_STREAM(NULL, usart_get_byte, _FDEV_SETUP_READ);
+
+#endif
